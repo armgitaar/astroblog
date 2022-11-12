ae90686f3839b119add0b90b96cd44d270e41a8f
Adam Miedema <armgitaar@yahoo.com>
2022-11-12 10:28:25 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,15 @@
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+});
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
